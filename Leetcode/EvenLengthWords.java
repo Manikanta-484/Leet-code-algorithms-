194f8ecd5d47faab91688424e463f043199a4ce9
Manikanta P <149045195+Manikanta-484@users.noreply.github.com>
2024-02-08 19:01:03 +0530

    p14

---
+++ Leetcode/EvenLengthWords.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+       String input="sky is blue and vast";
+       String[] wordsofString=input.split(" ");
+       for(String currentWord :wordsofString){
+           if(currentWord.length() % 2==0){
+               System.out.println(currentWord);
+           }
+       }
+    }
+}
